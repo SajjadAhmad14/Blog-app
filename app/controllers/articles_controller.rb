ca41134554d0cb3baa35ad76df9670097d56f0be
Sajjad Ahmad <jogimar14@gmail.com>
2020-10-15 19:10:55 +0500

    add flash to create and destroy actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = "Article '#{@article.title}' created!"
     redirect_to article_path(@article)
   end
 
@@ -32,7 +33,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    
+    flash.notice = "Article '#{@article.title}' deleted!"
     redirect_to articles_path
   end
 end
